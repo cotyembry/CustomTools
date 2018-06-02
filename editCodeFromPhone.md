26d6085f0fbebdb88cc59e855f72fda7fede6ea4
Coty Embry <cotyembry@gmail.com>
2018-06-02 11:36:35 -0500

    adding stable nrs.pl commit to add host and port if none is provided to run the `npm run start` npm script

---
+++ editCodeFromPhone.md
@@ -0,0 +1,20 @@
+want:
+
+        1-execute command to start a process that will:
+            1.1-listen to a file and when it is saved called "commitMessageAndPushOnChange.txt"
+            1.2-run a webpack command to properly compile the app to
+            1.3-make a commit message with the first line in the commitMessageAndPushOnChange.txt file
+            1.4-execute a git command to push to github
+            1.5-push to surge
+-------
+
+1.
+    
+    It should execute a file that Im about to write called
+        "compilejsPushLiveCommitAndPush.js"
+    
+    The command must have `npm run start` in it somewhere or something so that the webpack process can be started
+
+    
+    
+    
--- nrs.pl
+++ nrs.pl
@@ -23,32 +23,43 @@ for($i = 0; $i < $outputLength; $i++) {								#this for loop gets the ipaddress
 
 #now edit the package.json file to use the ipaddress
 open $packagejson, "./package.json" or die "Cannot open: $!\n";
-open $new, ">", "./package.jsonnew" or die "Cannot open: $!\n";
+open $new, ">", "./package.jsonjce" or die "Cannot open: $!\n";
 $foundWebserverLine = 0;
 while(<$packagejson>) {
-	if($_ =~ m/host\=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/gi) {						#this matches the host/ipaddress flag
-		$_ =~ s/host\=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/host=$ipaddressFinal/;
-		# print "$_\n\n\n";
+	# if($_ =~ m/host\=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/gi) {						#this matches the host/ipaddress flag
+	# 	$_ =~ s/host\=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/host=$ipaddressFinal/;
+	# 	print $new $_;
+	# 	$foundWebserverLine = 1;
+	# }
+	if($_ =~ m/\"start\"\:[ ]*\"webpack\-dev\-server/gi) {						#this matches the host/ipaddress flag
+		if($_ =~ m/host\=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/gi) {
+			$_ =~ s/host\=[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/host=$ipaddressFinal/;
+		}
+		elsif($_ =~ m/webpack\-dev\-server/gi) {
+			$_ =~ s/webpack\-dev\-server/webpack\-dev\-server \-\-host=$ipaddressFinal/;	#pass in the --host=n.n.n.n ipaddress
+
+			if($_ =~ m/port\=[0-9]+/gi) {}
+			else {																		#if --port=nnnn was not passed in add as default port=8080
+				$_ =~ s/webpack\-dev\-server/webpack\-dev\-server \-\-port\=8080 /;
+			}
+		}
+
 		print $new $_;
-		# print "$_\n";
 		$foundWebserverLine = 1;
 	}
 	else {
 		print $new $_;
-		# print "$_\n";
 	}
-
-	# print packagejson $_;
 }
 close $packagejson;
 close $new;
-#all the above block just did was create a new ./package.jsonnew file that has my current --host=... command line flag set for webpack :/
+#all the above block just did was create a new ./package.jsonjce file that has my current --host=... command line flag set for webpack :/
 
-# system "rm package.jsonnew";
+# system "rm package.jsonjce";
 
 #I couldn't figure out a better way, so I'm just getting it to work for now...I really need to come back and make these two while loops be only 1...and these two times for opening the file to only be once per file...oh well for now
 open $packagejson, ">", "./package.json" or die "Cannot open, ./package.json: $!\n";
-open $new, "./package.jsonnew" or die "Cannot open: $!\n";
+open $new, "./package.jsonjce" or die "Cannot open: $!\n";
 
 while(<$new>) {
 	# print $packagejson $_;
@@ -64,6 +75,6 @@ if($foundWebserverLine == 0) {
 	print "Error: did not find webserver line - in nrs.pl\n\n";
 }
 
-# system "type package.jsonnew";
+# system "type package.jsonjce";
 
 system "npm run start";	#this patches back in my usual `nrs` routine/habit
